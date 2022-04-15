68de976d92a49f53ee4b9270386473ea6f2e7a1d
jishnu-radhakrishnan <jishnurk10@gmail.com>
2022-04-15 20:40:22 +0530

    added2

---
+++ src/com/bridgelabz/problem002.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class problem002 {
+    public static void main(String[] args){
+        int x=10;
+        float y=5.3f;
+        char z='A';
+        boolean w=true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(w);
+    }
+}
+
