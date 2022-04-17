28f20f646b93267cc795d6d93b07fb91fb07f7f0
jishnu-radhakrishnan <jishnurk10@gmail.com>
2022-04-17 14:08:32 +0530

    added 3rd problem

---
+++ src/com/bridgelabz/problem003.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class problem003 {
+    public static void main(String[] args){
+       String x="JAVA";
+       String y="JAVA";
+       if(x == y)
+           System.out.println("Both strings are equal");
+       else
+           System.out.println("Both strings are not equal");
+    }
+}
